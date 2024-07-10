a7821b55aa99633cda1b8ecb185b62631179691e
Vadym <Servicepost.v@gmail.com>
2024-07-10 17:25:54 +0200

    syntax changes

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -1,7 +1,5 @@
 package example;
 
-import example.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
