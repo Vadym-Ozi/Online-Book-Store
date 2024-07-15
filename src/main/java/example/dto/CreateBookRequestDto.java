2a19d912246f0c5efc0aaf7a324cd7d25472b3cb
Vadym <Servicepost.v@gmail.com>
2024-07-15 17:04:19 +0200

    Used better annotation for String value in CreateBookRequestDto.java class.

--- src/main/java/example/dto/CreateBookRequestDto.java
+++ src/main/java/example/dto/CreateBookRequestDto.java
@@ -2,6 +2,7 @@ package example.dto;
 
 import java.math.BigDecimal;
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
@@ -10,14 +11,14 @@ import lombok.Setter;
 @Setter
 @Getter
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
     @NotNull
     @DecimalMin("0")
     private BigDecimal price;
-    @NotNull
+    @NotBlank
     @Pattern(regexp = "^[0-9]{13}$", message = "ISBN must be positive and contains 13 digits")
     private String isbn;
     private String description;
